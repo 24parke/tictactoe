00d7cba3d5086f0fa812787024b3a2bc5e448386
ethan <24parke>
2020-12-18 17:11:26 -0500

    made createboard less restrictive

--- main.py
+++ main.py
@@ -36,19 +36,26 @@ def flip_coin(name1, name2):
 
 def start():
     print("Each -- is an empty 'box'. Each box corresponds to a number, starting at 1 in the top left box, then going\
-from left to right. ")
+ from left to right. ")
     p1 = input("Player 1 name: ")
     p2 = input("Player 2 name: ")
     create_board()
     end = False
-    while not end:
+    #while not end:
 
+
+
+    #make board public, make board not constrained to 3 'boxes', instead, num of boxes based on user input
 
 def create_board():
-
+    board = ["--", "--", "--"]
     print("__________")
-    for i in range(3):
-        print("|--|--|--|")
+    for i in range(len(board)):
+        for i in range(len(board)):
+            if i == len(board) - 1:
+                print("|" + board[i] + "|" + "\n", end = "")
+            else:
+                print("|" + board[i], end = "")
     print("----------\n")
     # print("--------")
     # for i in range(3):
@@ -62,7 +69,7 @@ def play(starting_player, name1, name2):
         choice = input(starting_player + ": ")
         try:
             choice = int(choice)
-            if choice <= 0 or choice >= 10:
+            if choice <= 0 or choice >= len(board) * len(board):
                 choice = input("Please choose an integer between 1 and 9")
 
         except ValueError:
@@ -71,6 +78,8 @@ def play(starting_player, name1, name2):
 
 
 
+
+create_board()
 start()
 
 
